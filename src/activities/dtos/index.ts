fad3b2575b6484bf56def93d788577535d81c512
Vlad Morozov <vlad_morozov@pecodesoftware.com>
2026-02-04 12:33:32 +0100

    fix

--- src/activities/dtos/index.ts
+++ src/activities/dtos/index.ts
@@ -2,6 +2,6 @@ export * from './ActivityGroupPayload.dto';
 export * from './ActivityGroupResponse.dto';
 export * from './ActivityPayload.dto';
 export * from './ActivityResponse.dto';
-export * from './SubActivityPayload,dto';
+export * from './SubActivityPayload.dto';
 export * from './SubActivityQuery.dto';
 export * from './SubActivityResponse.dto';
